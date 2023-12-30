20eb2726d32c92959983bf643ea724504c2296b2
babaprincedaouda71 <babadaouda01@gmail.com>
2023-12-30 13:17:41 +0100

    konan-07-30-12-2023

--- be/src/main/java/org/example/be/BeApplication.java
+++ be/src/main/java/org/example/be/BeApplication.java
@@ -1,13 +1,17 @@
 package org.example.be;
 
+import org.example.be.dto.CustomerRequestDTO;
+import org.example.be.dto.CustomerResponseDTO;
 import org.example.be.dto.PerfumeRequestDTO;
 import org.example.be.dto.PerfumeResponseDTO;
+import org.example.be.service.CustomerService;
 import org.example.be.service.PerfumeService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootApplication
@@ -18,9 +22,9 @@ public class BeApplication {
 	}
 
 	@Bean
-	CommandLineRunner start(PerfumeService perfumeService){
+	CommandLineRunner start(PerfumeService perfumeService, CustomerService customerService){
 		return args -> {
-//			perfumeService.addPerfume(new Perfume(null, "One Million", "Sensadffjfjerkjdfvdfkjvnfdj", 20));
+			System.out.println("******************** PERFUMES ******************");
 			for (int i = 0; i < 150; i++) {
 				PerfumeRequestDTO perfumeRequestDTO = PerfumeRequestDTO.builder()
 						.name("ABC" + i)
@@ -31,8 +35,35 @@ public class BeApplication {
 				perfumeService.addPerfume(perfumeRequestDTO);
 			}
 
+			System.out.println("-------------------- PERFUMES LIST --------------------");
 			List<PerfumeResponseDTO> perfumes = perfumeService.getPerfumes();
 			perfumes.forEach(System.out::println);
+
+			System.out.println("////////////////////////////////////////////////");
+			System.out.println("////////////////////////////////////////////////");
+			System.out.println("////////////////////////////////////////////////");
+
+			System.out.println("******************** CUSTOMERS ******************");
+			CustomerRequestDTO customerRequestDTO = CustomerRequestDTO.builder()
+					.firstName("Baba")
+					.lastName("Prince")
+					.addressMail("iambabaprince@gmail.com")
+					.phoneNumber("+212693823094")
+					.build();
+
+			CustomerRequestDTO customerRequestDTO1 = CustomerRequestDTO.builder()
+					.firstName("Boris")
+					.lastName("Samne")
+					.addressMail("borissamne@gmail.com")
+					.phoneNumber("+212693823094")
+					.build();
+
+			customerService.addCustomer(customerRequestDTO);
+			customerService.addCustomer(customerRequestDTO1);
+
+			System.out.println("-------------------- CUSTOMERS LIST --------------------");
+			List<CustomerResponseDTO> customers = customerService.getCustomers();
+			customers.forEach(System.out::println);
 		};
 	}
 
