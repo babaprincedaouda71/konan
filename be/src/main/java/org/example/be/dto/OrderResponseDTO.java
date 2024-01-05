12ef389b22de9c20964d3ae279fceb001527185d
babaprincedaouda71 <babadaouda01@gmail.com>
2024-01-05 11:08:42 +0100

    konan-dtoorder-01 05/01/2024

--- be/src/main/java/org/example/be/dto/OrderResponseDTO.java
+++ be/src/main/java/org/example/be/dto/OrderResponseDTO.java
@@ -1,12 +1,9 @@
 package org.example.be.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.example.be.entities.Customer;
 
-@Data @NoArgsConstructor @AllArgsConstructor @Builder
+@Data
 public class OrderResponseDTO {
     private Long id;
+    private CustomerResponseDTO customerResponseDTO;
 }
